079d3322aa8a0266591e080ea3fc343ed0459f20
Jackmc <jackmc1122@gmail.com>
2019-08-27 14:49:07 -0700

    Refactor and fix uppercase issue.

--- src/containers/words/TopWords.js
+++ src/containers/words/TopWords.js
@@ -1,10 +1,9 @@
 import { connect } from 'react-redux';
 import Words from '../../components/words/Words';
-import { getFirstWords, getTopWordsByTerm } from '../../selectors/wordsSelectors';
+import { getFirstWords } from '../../selectors/wordsSelectors';
 
 const mapStateToProps = (state, props) => ({
   words: getFirstWords(state, props.match.params.count || 1000)
-  // words: getTopWordsByTerm(state)
 });
 
 export default connect(
--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -1,9 +1,5 @@
 export const getWords = state => state.words.words;
 export const getFirstWords = (state, numWords = 1000) => getTopWordsByTerm(state).slice(0, numWords);
 export const getSearchTerm = state => state.words.searchTerm;
-export const getTopWordsByTerm = state => {
-  const words = getWords(state);
-  const term = getSearchTerm(state);
-  const filtered = words.filter(word => word.includes(term));
-  return filtered;
-};
+export const getTopWordsByTerm = state => getWords(state)
+  .filter(w => w.includes(getSearchTerm(state).toLowerCase()));
