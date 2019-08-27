096fa635028486e51f99cc72a8d2e59520f0eaee
Jackmc <jackmc1122@gmail.com>
2019-08-27 13:20:45 -0700

    Fix eslint spacing stuff.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import store from './store';
 import App from './components/App';
 
 render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
